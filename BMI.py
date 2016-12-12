c268aafc0ecdc3258757ea11a1ece1d995ffe0d0
vuhoang1998 <rabac1998@gmail.com>
2016-12-12 16:26:01 +0700

    Create BMI.py

---
+++ BMI.py
@@ -0,0 +1,35 @@
+# Homework-session-6from flask import Flask,render_template,redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/school')
+def techkids():
+    return redirect("http://techkids.vn")
+
+@app.route('/BMI/<int: weight>/<int : height>')
+def BMI_caculate(): 
+    BMI = weight * 10000 / (height ** 2)
+    if BMI < 16:
+        result = "Severely underweight (còi xương)"
+    elif 16 < BMI <= 18.5:
+        result = "Underweight (Thiếu cân)"
+    elif 18.5 < BMI <= 25:
+        result = "Normal (Bình thường)"
+    elif 25 < BMI <= 30:
+        result = "Overweight (Thừa cân)"
+    else:
+        result = "Obese (Béo phì)"
+    return render_template("BMI.html")
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
